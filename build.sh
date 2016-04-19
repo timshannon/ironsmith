7f94b454a73f4a66a5c4491fe55971fb96cace10
Tim Shannon <shannon.timothy@gmail.com>
2016-04-19 19:26:35 +0000

    work on build scripts

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-go-bindata web/... && go build -a -v -o ironsmith -pkgdir .
+mkdir -p ./workspace/src
+export GOPATH=./workspace
 
+git clone https://git.townsourced.com/tshannon/config.git ./workspace/src
+
+go-bindata web/... && go build -a -v -o ironsmith -pkgdir ./workspace
