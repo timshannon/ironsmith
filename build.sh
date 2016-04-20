9632d7f2b60b953ef51a2114809a2e61a14b3216
Tim Shannon <shannon.timothy@gmail.com>
2016-04-20 16:22:42 +0000

    Post trigger test

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ dir=$1
 
 go get -u git.townsourced.com/townsourced/ironsmith
 
-cd $dir/src/git.townsourced.com/townsourced/ironsmith
+ls $dir
+#cd $dir/src/git.townsourced.com/townsourced/ironsmith
 
 go-bindata web/... && go build -a -v -o ironsmith
