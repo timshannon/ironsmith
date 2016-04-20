066ae7aa32e576d8478281735f029bcda3ed0bfa
Tim Shannon <shannon.timothy@gmail.com>
2016-04-20 16:42:16 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-dir=$1
-
 go get -u git.townsourced.com/townsourced/ironsmith
-
-ls $dir
-#cd $dir/src/git.townsourced.com/townsourced/ironsmith
 
 go-bindata web/... && go build -a -v -o ironsmith
